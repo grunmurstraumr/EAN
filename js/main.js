0d7e552748e4bf4eeb51c77721de1696262cbbeb
Erik <erik.grundstrom@hotmail.com>
2019-04-07 00:11:45 +0200

    Lot's of minor refactoring

--- js/main.js
+++ js/main.js
@@ -1,22 +1,89 @@
 /*************************************************************************
 * Class definitions
 *************************************************************************/
+
 class Entry{
     constructor(name, plu, ean, image_url, active){
         this.name = name;
-        this.plu = plu,
-        this.ean = ean
-        this.image = image_url
-        this.active = active
+        this.plu = plu;
+        this.ean = ean;
+        this.image = image_url;
+        this.active = active;
+        this.changed = true; //Flag to detect if redrawing would be necessary
+        this.id = name.replace(/[\s\/]/g, '-'); // Replace spaces with hyphens
     }
     fields(){
         //Return an array with the data fields
         return [this.name, this.plu, this.ean, this.image, this.active];
     }
+
+    edit(property, value){
+        changed = true;
+        if (property === 'name' || property ==='id'){
+            this.old_id = this.id;
+        }
+        try{
+            this[property] = value
+            if (property === 'name'){
+                
+            }
+        }
+        catch(error){
+            console.error(`${property} is an unknown property.`);
+        }
+
+    }
+    toHtml(parent){
+        // If nothing is changed just skip the entire function.
+        if (this.changed){
+            try {
+                let id;
+                try {
+                    id = this.old_id;
+                }
+                catch(error){
+                    id = this.id;
+                }
+                let self_HTML = document.querySelector(`#${id}`);
+                parent.removeChild(self_HTML);
+            }
+            catch(error){
+                // Assume that only the removal of self failed and silence error.
+            }
+            finally{
+                let html = document.createElement('div');
+                html.classList.add('item');
+                if (strToBool(this.active))
+                    html.classList.add('active');
+                let img = document.createElement('img');
+                img = assign_image(this.image);
+                html.innerHTML = `
+                    <h2> ${this.name} </h2>
+                    <p> ${this.plu}</p>
+                    `
+                let canvas = document.createElement('canvas');
+                canvas.id = `${this.id}_barcode`;
+                html.appendChild(img);
+                html.appendChild(canvas);
+                html.addEventListener('click', () => {
+                    html.classList.toggle('active');
+                    this.active = strToBool(this.active) ? 'false' : 'true'; //Toggle true/false for active 'flag'
+                    console.log(this); // DEBUG. Remove for production
+                });
+                parent.appendChild(html);
+                JsBarcode(`#${this.id}_barcode`, this.ean, {
+                    height: settings['jsbarcode-height'],
+                    width: settings['jsbarcode-width'],
+                    displayValue: settings['display-value'],
+                });
+                this.changed = false;
+            }
+        }
+    }
 }
 
 class Dialog{
-    // Maybe this can be used to render itself as well based on input
+    // Maybe this can be used to render itself as well based on input?
     constructor(name, id){
         this.prop_name = name;
         this.prop_id = id;
@@ -26,6 +93,10 @@ class Dialog{
     }
     get id (){
         return `#${this.prop_id}`;
+    }
+
+    toHtml(parent){
+
     }
 
 }
@@ -70,37 +141,6 @@ settings['card-height'] = 2*settings['card-padding']+settings['barcode-height'] 
 *************************************************************************/
 let data = []
 
-
-/*************************************************************************
-* Windowing
-*************************************************************************/
-let window_stack = [];
-
-/*************************************************************************
-* Rendering
-*************************************************************************/
-
-
-/*************************************************************************
-* Utility functions
-*************************************************************************/
-const strToBool = str => {
-    const truthy = ['true', '1'];
-    return truthy.indexOf(str.toLowerCase()) !== -1;
-}
-
-
-
-
-let find_data = (value, key) =>{
-    //Searches in data for entry on specified key-value pair
-    return data.find((element) =>{
-        return element[key].trim() === value.trim()
-    });
-}
-
-
-
 const assign_image = (URL) =>{
     let image = new Image();
     image.onerror = () =>{
@@ -110,51 +150,12 @@ const assign_image = (URL) =>{
     return image;
 }
 
-function render(){
-    document.getElementById('items').innerHTML = "";
-    let root = document.getElementById('items');
-    
-    for (let i = 0; i < data.length; ++i){
-        let item = data[i];
-        let html = document.createElement('div');
-        let id = item.name.replace(/[\s\/]/g, '-') // Replaces spaces with hyphens
-        html.classList.add('item');
-        img = document.createElement('img');
-        img = assign_image(item.image);
-        html.innerHTML =`
-            <h2> ${item.name} </h2>
-            <p> ${item.plu} </p>
-            `
-        canvas = document.createElement('canvas');
-        canvas.id = `${id}_barcode`;
-        html.appendChild(img);
-        html.appendChild(canvas);
-        root.appendChild(html);
-        html.addEventListener('click', () => {
-            html.classList.toggle('active');
-            data_representation = find_data(html.querySelector('p').innerHTML, 'plu')
-            console.log(find_data(html.querySelector('p').innerHTML, 'plu'));
-           
-            data_representation.active = strToBool(data_representation.active) ? 'false' : 'true' ;
-            console.log(`HTML: ${html}\nDATA: ${data_representation}`);
-        });
-
-        JsBarcode(`#${id}_barcode`, item.ean, {
-            height: settings['jsbarcode-height'],
-            width: settings['jsbarcode-width'],
-            displayValue: settings['display-value'],
-        });
-        if (strToBool(item.active))
-            html.classList.add('active')
-    }
-}
-
 function load_data(){
     clear_all();
     let file = document.querySelector("#data_files input").files[0];
     let reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = () =>{
+
+    reader.onload = (e) =>{
         // Split file into array of data entries
         let entries = reader.result.split('\n');
         //Iterate over array and split each into sub-arrays of values
@@ -175,9 +176,10 @@ function load_data(){
                 return 1
             else
                 return -1});
-        render();
-    }
+        renderAll();
 
+    }
+    reader.readAsText(file);
 }
 
 function load_config(){
@@ -186,9 +188,7 @@ function load_config(){
     reader.readAsText(file);
     reader.onload = () =>{
         // Split configurations into an array of lines
-        console.log(reader.result)
         let pairs = reader.result.split('\n');
-        console.log(pairs)
         for (let i = 0; i < pairs.length; ++i){
             // Split each line into arrays consisting of key and value
             // Key is index 0 and value is index 1
@@ -200,24 +200,9 @@ function load_config(){
 
 }
 
-const print_page = () => {
-    let selected = document.querySelectorAll('.active');
-    let unselected = document.querySelectorAll('.item:not(.active)');
-    for (let item of unselected){
-        item.classList.add('hidden');
-    }
-    window.print();  
-    for (let item of unselected){
-        item.classList.remove('hidden');
-    }
-}
-
-let get_fields_from_html = (item) => {
-    //Returns an array with the data fetched from the html
-    // [name, plu, ean, image_url, active]
-    let search_key = item.querySelector('p').innerHTML.trim() // Search on plu, not name, Very hacky solution. Needs refactoring
-    let data_item = find_data(search_key, 'plu');
-    return data_item.fields();
+const clear_all = () =>{
+    document.querySelector('#items').innerHTML = "";
+    data = [];
 }
 
 let save_data = () =>{
@@ -242,39 +227,6 @@ let save_data = () =>{
     
     let output = "";
     for (let item of data_items){
-        //console.log(item)
-        output += `${format_csv(item)}\n`
-    }
-    download_anchor.setAttribute('href', 'data:text/plain;encoding=utf-8,' + encodeURIComponent(output));//'data:application/octet-stream,' + encodeURIComponent(output));
-    download_anchor.setAttribute('download', settings['default-save-filename']);
-    document.body.appendChild(download_anchor);
-    download_anchor.click();
-    document.body.removeChild(download_anchor);
-    
-}
-let save_active = () =>{
-    // Use a data-URI anchor to create a downloadable file
-    let format_csv = (str_arr) => {
-        output = "";
-        for (let value of str_arr){
-            output += `${value}; `
-        }
-        return output;
-    }
-    // Set up download anchor
-    var download_anchor = document.createElement('a');
-    download_anchor.style.display = 'none';
-    // Get active items from html
-    let active_items = Array.from(document.querySelectorAll('.active'));
-
-    for (let i = 0; i < active_items.length; ++i){
-        active_items[i] = get_fields_from_html(active_items[i]);
-    }
-    //active_items.map(get_fields_from_html);
-    
-    let output = "";
-    for (let item of active_items){
-        //console.log(item)
         output += `${format_csv(item)}\n`
     }
     download_anchor.setAttribute('href', 'data:text/plain;encoding=utf-8,' + encodeURIComponent(output));//'data:application/octet-stream,' + encodeURIComponent(output));
@@ -285,8 +237,26 @@ let save_active = () =>{
     
 }
 
+let find_data = (value, key) =>{
+    //Searches in data for entry on specified key-value pair
+    return data.find((element) =>{
+        return element[key].trim() === value.trim()
+    });
+}
 
+let get_fields_from_html = (item) => {
+    //Returns an array with the data fetched from the html
+    // [name, plu, ean, image_url, active]
+    let search_key = item.querySelector('p').innerHTML.trim() // Search on plu, not name, Very hacky solution. Needs refactoring
+    let data_item = find_data(search_key, 'plu');
+    return data_item.fields();
+}
+
+/*************************************************************************
+* Windowing
+*************************************************************************/
 let dialogs = {'load_dialog' : 'data_file_dialog',};
+let window_stack = [];
 
 const show_dialog = dialog_id => {
     let dialog = document.querySelector(`#${dialog_id}`);
@@ -295,40 +265,85 @@ const show_dialog = dialog_id => {
     dialog.focus();
 
 }
-const close_window = () => {
+const close_top_window = () => {
     let current = window_stack.pop();
     current.classList.add('hidden');
 }
 
-const clear_all = () =>{
-    document.querySelector('#items').innerHTML = "";
-    data = [];
-}
-
-const escape_listener = (event) =>{
-    if(event.key === 'Escape'){
-        event.preventDefault();
-        close_window();
+/*************************************************************************
+* Rendering
+*************************************************************************/
+function renderAll(){
+    document.getElementById('items').innerHTML = "";
+    let root = document.getElementById('items');
+    for (let i = 0; i < data.length; ++i){
+        data[i].toHtml(root);
     }
 }
-const setup_event_listeners = ()=>{
-    document.addEventListener('keydown', escape_listener)
-    document.querySelector('#data_files button').addEventListener('click',() => {
-        load_data();
-        close_window();
-    })
-    document.querySelector('#command_load_data_btn').addEventListener('click', () =>{
-        show_dialog('data_file_dialog');
-    })
-    
+
+const print_page = () => {
+    let selected = document.querySelectorAll('.active');
+    let unselected = document.querySelectorAll('.item:not(.active)');
+    // Hide all elements not selected
+    for (let item of unselected){
+        item.classList.add('no-print');
+    }
+    // Open print dialog. This is a blocking operation so the following for
+    // loop is not executed until the print dialog is closed.
+    window.print();  
+    // Un-hide previously hidden items that were not selected for printing
+    for (let item of unselected){
+        item.classList.remove('no-print');
+    }
 }
 
+
+/*************************************************************************
+* Utility functions
+*************************************************************************/
+const strToBool = str => {
+    const truthy = ['true', '1'];
+    return truthy.indexOf(str.toLowerCase()) !== -1;
+}
+
+/*************************************************************************
+* Event handlers
+*************************************************************************/
+const handle_key_press = (event) =>{
+    if(event.key === 'Escape'){
+        event.preventDefault();
+        close_top_window();
+    }
+}
+
+/*************************************************************************
+* Event Listeners
+*************************************************************************/
+
+const setup_event_listeners = ()=>{
+    document.addEventListener('keydown', handle_key_press)
+    document.querySelector('#data_files input').addEventListener('change', (event)=>{
+        if(event.target.files.length > 0)
+            load_data();
+
+        close_top_window();
+    });
+    document.querySelector('#load_data_btn').addEventListener('click', () =>{
+        show_dialog('data_file_dialog');
+    });
+    document.querySelector('#printable_btn').addEventListener('click', () => {
+        print_page();
+    });
+    document.querySelector('#save_data_btn').addEventListener('click', () => {
+        save_data();
+    });
+}   
 document.addEventListener('DOMContentLoaded', () => {
     // Set a periodic interval to communicate with server that 
     // the application is alive. If not the server should quit
     // after a specified time
     // setInterval()
     setup_event_listeners();
-    show_load_window();
+    show_dialog('data_file_dialog');
 });
 
